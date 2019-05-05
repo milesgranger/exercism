0350d4feb7058bd2c7ab899842f50f82b5bdad3c
Miles Granger <miles59923@gmail.com>
2019-05-05 13:07:15 +0200

    Checkpoint - sum of multiples

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,8 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int): int =
+        {0..upperBound + 1}
+        |> Seq.filter (fun n -> numbers
+                                |> List.exists (fun number -> n % number = 0))
+        |> Seq.sum
+        
